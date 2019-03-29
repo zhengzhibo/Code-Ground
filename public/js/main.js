774dc652e983a5f19a4adb6483458ad0a723e0fb
Alex Zheng <zhibo90@gmail.com>
2019-03-29 23:45:21 +0800

    test layout

--- public/js/main.js
+++ public/js/main.js
@@ -1,11 +1,28 @@
-require.config({ paths: { 'vs': 'monaco-editor/min/vs' }});
-require(['vs/editor/editor.main'], function() {
-    var editor = monaco.editor.create(document.getElementById('container'), {
-        value: [
-            'function x() {',
-            '\tconsole.log("Hello world!");',
-            '}'
-        ].join('\n'),
-        language: 'javascript'
-    });
+// require.config({ paths: { 'vs': 'monaco-editor/min/vs' }});
+// require(['vs/editor/editor.main'], function() {
+//     var editor = monaco.editor.create(document.getElementById('container'), {
+//         value: [
+//             'function x() {',
+//             '\tconsole.log("Hello world!");',
+//             '}'
+//         ].join('\n'),
+//         language: 'javascript'
+//     });
+// });
+
+Split(["#a", "#b"], {
+  gutterSize: 8,
+  cursor: "col-resize"
+});
+Split(["#c", "#d"], {
+  direction: "vertical",
+  sizes: [25, 75],
+  gutterSize: 8,
+  cursor: "row-resize"
+});
+Split(["#e", "#f"], {
+  direction: "vertical",
+  sizes: [25, 75],
+  gutterSize: 8,
+  cursor: "row-resize"
 });
