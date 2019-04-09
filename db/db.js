8cac2cb4b581b5bd52b3ba6042f7c439bf8a767f
zhengzhibo <zhibo90@gmail.com>
2019-04-09 11:18:42 +0800

    typo

--- db/db.js
+++ db/db.js
@@ -1,5 +1,5 @@
 const low = require('lowdb');
-const FileASync = require('lowdb/adapters/FileASync');
+const FileASync = require('lowdb/adapters/FileAsync');
 const common = require('../common');
 
 const dburl = common.db;
