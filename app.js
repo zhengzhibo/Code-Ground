59f00d982bf8f58e2292b3e00d3ca1d0ca5ccfc3
zhengzhibo <zhibo90@gmail.com>
2019-04-09 18:24:04 +0800

    add skip log

--- app.js
+++ app.js
@@ -39,7 +39,9 @@ app.use(session({
 }))
 
 
-app.use(logger('dev'));
+app.use(logger('dev', {
+  skip: function (req, res) { return res.statusCode < 400 }
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
